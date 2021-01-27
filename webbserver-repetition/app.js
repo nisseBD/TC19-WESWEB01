b515f59cbd95d5f5de58b26a107bcb67a94df9b8
nisseBD <70211737+nisseBD@users.noreply.github.com>
2021-01-27 16:20:15 +0100

    init

--- webbserver-repetition/app.js
+++ webbserver-repetition/app.js
@@ -2,6 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log("Connected")
+});
+
+const namnListaSchema = new mongoose.Schema({
+  names: Array,
+});
+
+const NamnLista = mongoose.model('NamnLista', namnListaSchema);
+
 app.use(express.static(__dirname + "\\client"));
 
 app.use(express.json());
@@ -13,6 +30,17 @@ const namn = [];
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { names: namn });
+});
+
+app.get("/save", (req, res) => {
+  var nameList = NamnLista({
+    names: namn,
+  });
+
+  nameList.save();
+
+  namn.splice(0, namn.length);
+  res.redirect("/");
 });
 
 app.post("/", function (req, res) {
