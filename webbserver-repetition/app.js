55328fce52c9490f6928842cca3fc0b84ad644f9
NiklasHj <54146045+NiklasHj@users.noreply.github.com>
2021-01-27 16:03:28 +0100

    sad

--- webbserver-repetition/app.js
+++ webbserver-repetition/app.js
@@ -1,25 +1,23 @@
 const express = require("express");
-const { set } = require("mongoose");
 const app = express();
 const port = 3000;
 
 app.use(express.static(__dirname + "\\client"));
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.set("view engine", "ejs");
 
-const namn = [
-  "Niklas",
-  "Nils",
-  "AliG",
-  "AliF",
-  "Johanna",
-  "Liam",
-  "Samira",
-  "Vinnie",
-];
+const namn = [];
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { names: namn });
+});
+
+app.post("/", function (req, res) {
+  namn.push(req.body.firstName);
+  res.redirect("/");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
